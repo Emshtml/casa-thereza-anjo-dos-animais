3793ecb2521c1c55db51ca69837f9818d1cb6576
Emshtml <casadathereza1@gmail.com>
2025-10-13 18:06:44 -0300

    Create Footer.js

---
+++ pages/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-primary text-white py-6 mt-8 text-center">
+      <p>© {new Date().getFullYear()} Casa da Thereza Anjo dos Animais. 🐾 Todos os direitos reservados.</p>
+    </footer>
+  )
+}
