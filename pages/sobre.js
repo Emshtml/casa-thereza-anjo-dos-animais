9d10482a785ba28ddbcba27118a6115e54a6ba38
Emshtml <casadathereza1@gmail.com>
2025-10-18 19:56:18 -0300

    Create sobre.js

---
+++ pages/sobre.js
@@ -0,0 +1,13 @@
+import Layout from "../components/Layout";
+
+export default function Sobre() {
+  return (
+    <Layout>
+      <h2 className="text-2xl font-bold mb-4">Nossa História 🐾</h2>
+      <p>
+        A Casa Thereza Anjo dos Animais nasceu do amor e dedicação à causa animal.
+        Acolhemos, tratamos e encontramos lares responsáveis para animais em situação de vulnerabilidade.
+      </p>
+    </Layout>
+  );
+}
