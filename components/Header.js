3100b5dd48f1f3e96851520299e912972f0a3959
Emshtml <casadathereza1@gmail.com>
2025-10-19 20:02:40 -0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,12 @@
-import Link from "next/link";
-
 export default function Header() {
   return (
-    <header className="bg-amber-200 py-4 shadow-md">
-      <nav className="flex justify-center space-x-6 font-semibold text-gray-700">
-        <Link href="/">Início</Link>
-        <Link href="/sobre">Sobre</Link>
-        <Link href="/adocao">Adoção</Link>
-        <Link href="/ajudar">Como Ajudar</Link>
-        <Link href="/galeria">Galeria</Link>
-        <Link href="/eventos">Eventos</Link>
-        <Link href="/contato">Contato</Link>
+    <header className="header-promo">
+      <h1 className="header-logo">Casa Thereza 🐾</h1>
+      <nav className="header-nav">
+        <a href="/">Início</a>
+        <a href="/sobre">Sobre</a>
+        <a href="/adocao">Adoção</a>
+        <a href="/contato">Contato</a>
       </nav>
     </header>
   );
