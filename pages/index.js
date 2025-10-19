ba308be59a069571b631faf84e796d3ef67e3059
Emshtml <casadathereza1@gmail.com>
2025-10-19 10:42:39 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
       <section className="text-center">
         <h2 className="text-3xl font-bold mb-4">Bem-vindo à Casa Thereza 🐾</h2>
         <p className="text-lg mb-6">
-          Um abrigo de amor e esperança para cães, gatos e coelhos em busca de um lar.
+          Um abrigo de amor e esperança para cães e gatos  em busca de um lar.
         </p>
         <img
           src="/animais/cachorro1.jpg"
